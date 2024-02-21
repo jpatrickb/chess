684061dec65461d19ce11615bfa65fae88d7a674
Patrick Beal <jpbeal@byu.edu>
2024-02-20 21:18:10 -0700

    Working on User RegistrationService

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,9 @@
 package server;
 
+import com.google.gson.Gson;
+import model.User;
 import org.eclipse.jetty.websocket.server.WebSocketHandler;
+import server.Service.RegistrationService;
 import spark.*;
 
 import java.nio.file.Paths;
@@ -18,13 +21,25 @@ public class Server {
 
 
         // Register your endpoints and handle exceptions here.
-
+        Spark.post("/user", this::registerUser);
 
         Spark.awaitInitialization();
         return Spark.port();
     }
 
+
+    public int port() {
+        return Spark.port();
+    }
+
     public void stop() {
         Spark.stop();
+    }
+
+
+    private Object registerUser(Request request, Response response) {
+        var user = new Gson().fromJson(request.body(), User.class);
+        user = RegistrationService.registerUser(user);
+        return new Gson().toJson(user);
     }
 }
--- server/src/main/java/server/Service/RegistrationService.java
+++ server/src/main/java/server/Service/RegistrationService.java
@@ -1,4 +1,10 @@
 package server.Service;
 
+import model.User;
+
 public class RegistrationService {
+    public static User registerUser(User user) {
+//        Todo: actually build the function
+        return new User(null, null, null);
+    }
 }
